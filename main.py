d91d83221e00ea1024a194c9b36eb48a191ee80d
void4 <void4@users.noreply.github.com>
2021-02-03 19:27:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from time import sleep
 pygame.init()
 pygame.display.set_caption("my title")
 
-screen = pygame.display.set_mode((640,480))
+w = 640
+h = 480
 
-color = (255, 255, 255)
+screen = pygame.display.set_mode((w,h))
+
+color = (0, 0, 0)
 
 i = 0
 running = True
 while running:
 	screen.fill(color)
-	pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(i, i, 40, 30))
+	
+	#pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(i, i, 40, 30))
 	i += 1
 
 	for event in pygame.event.get():
