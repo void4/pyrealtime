f10bb1cd095031480263416688bf49e532347998
void4 <void4@noreply.github.com>
2022-04-06 11:11:43 +0200

    Add mouse and key

--- main.py
+++ main.py
@@ -15,13 +15,18 @@ i = 0
 running = True
 while running:
 	screen.fill(color)
-	
+
 	#pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(i, i, 40, 30))
 	i += 1
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+		elif event.type == pygame.MOUSEBUTTONUP:
+			pos = pygame.mouse.get_pos()
+			print(pos)
+
+	print(pygame.key.get_pressed()[pygame.K_UP])
 
 	pygame.display.flip()
 	sleep(0.1)
